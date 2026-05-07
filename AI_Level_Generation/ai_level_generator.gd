85b687de2b271a39db856d4b84fff9d3a44e48f3
Hazemnabil_42 <124460984+Hazem-nabil42@users.noreply.github.com>
2026-05-07 18:29:32 +0300

    Update ai_level_generator.gd

--- AI_Level_Generation/ai_level_generator.gd
+++ AI_Level_Generation/ai_level_generator.gd
@@ -11,7 +11,7 @@ signal generation_failed(error: String, response: String)
 @export var use_auto_path: bool = true
 
 # ✅ المتغيرات الناقصة
-const BASE_URL: String = "http://127.0.0.1:8000/api"
+const BASE_URL: String = "https://mario_backend.hf.space/api"
 var http_load: HTTPRequest = null
 var http_generate: HTTPRequest = null
 
